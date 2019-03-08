b72a88575325e0199a72ff82062e34f53a43268b
4330train <4330train@St-Matthews.lan>
2019-03-07 20:34:00 -0500

    Creating a level up function for the player * needs work*

---
+++ Base.py
@@ -0,0 +1,23 @@
+class myCharacter():
+    Default_NAME = ''
+    Defaut_health = 100.0
+    Default_strength = 10.0
+    Default_exp = 0.0
+    Default_mana = 0.0
+    Default_class = None
+
+def create_NAME():
+    if(myCharacter.Default_NAME == '' or None):
+        print('you need a name for your character.')
+        getting_Name = input('Your new name: ')
+        myCharacter.Default_NAME = getting_Name
+        print('your new name is {}', format(myCharacter.Default_NAME))
+    else:
+        # create battle function to continue !
+        print('Hello {}', format(myCharacter.Default_NAME))
+
+
+class Mage():
+    health = myCharacter.De
+    mana
+    stength
