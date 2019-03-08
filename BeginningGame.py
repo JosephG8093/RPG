53561a21e581ce2889a62b6ec8b03888659b185b
4330train <4330train@St-Matthews.lan>
2019-03-07 21:39:20 -0500

    made changes...

--- BeginningGame.py
+++ BeginningGame.py
@@ -1,6 +1,7 @@
 import os
 import time
-from Base.py import myCharacter, create_NAME
+from Base import myCharacter
+form Base import create_NAME
 from lvlUP import LevelUP
 from Classes import Mage, Warrior, Hunter
 
@@ -19,15 +20,16 @@ def start_Game():
     print('*W* Warrior')
     print('*H* Hunter')
     select_Class = input()
-    if(select_Class = 'M'):
+    if(select_Class == 'M'):
         myCharacter.Default_class == Classes.Mage
-    elif(select_Class = 'W'):
+    elif(select_Class == 'W'):
         myCharacter.Default_class == Classes.Warrior
-    elif(select_Class = 'H'):
+    elif(select_Class == 'H'):
         myCharacter.Default_class == Classes.Hunter
     else:
         print('There was a error, please type M,W,H to select a class')
         select_Class()
-    print('you have selected the class:', format(myCharacter.Default_class))
-    time.sleep(2.5)
-    clear()
+
+        print('You have selected: {}', format(myCharacter.Default_class))
+        time.sleep(2.5)
+        clear()
