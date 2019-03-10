637196a80da1a926a3d4c90e7daab571e2c0d1bf
4330train <4330train@St-Matthews.lan>
2019-03-09 19:37:37 -0500

    working on optomized version to make it a lot cleaner. Trashing other files later

---
+++ optomized/Character.py
@@ -0,0 +1,28 @@
+import backworker
+
+class player():
+    isAlive = True
+    Level = 1
+    Name = None
+    Strength = 1.7
+    Experience = 0
+
+    @classmethod
+    def battle(self, me, enemy):
+        self.me = player()
+
+        while(self.me.isAlive = True):
+
+class enemey_Imp():
+    isAlive = False
+    health = 10
+    Level = 1
+    Name = 'Imp'
+    Strength = 0.7
+    Quality = 'WEAK'
+
+    @classmethod
+    def display_info():
+        while(player.isAlive = True):
+            if(player.Level <= 5):
+                # NOTE: need more info to continue
---
+++ optomized/backworker.py
@@ -0,0 +1,3 @@
+import Character
+
+def display_Screen():
